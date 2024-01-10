fab644fbce49bfe50f7b553b23bdc5dc4dc4f6cf
Rahul Rasve <rahulrasve712@gmail.com>
2024-01-10 16:13:55 +0530

    add project files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#219EBC",
+        primaryDark: "#023047",
+        primaryLight: "#8ECAE6",
+        secondary: "#FB8500",
+        secondaryLight: "#FFB703",
+        // surface: "#F7f5f4",
+        // background: "#fefefe",
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
