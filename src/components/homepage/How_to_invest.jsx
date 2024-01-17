ee26ebe2a17eefc99e28c366d5485a7f6590f32a
Rahul Rasve <rahulrasve712@gmail.com>
2024-01-17 13:44:57 +0530

    hot-to-invest component in progress

---
+++ src/components/homepage/How_to_invest.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HowToInvest = () => {
+	return (
+		<div className="w-full bg-gray-50">
+			<div>
+				<h1 className="text-5xl flex ">HOW TO INVEST</h1>
+			</div>
+		</div>
+	);
+};
+
+export default HowToInvest;
