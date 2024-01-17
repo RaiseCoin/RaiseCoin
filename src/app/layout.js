79515e7de72ff9917dafab26cc484b5a347f505e
sumithprabhu <sumithprabhu07@gmail.com>
2024-01-18 00:38:31 +0530

    added contact us page, but routing not done yet

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import NavigationBar from "@/components/navbar/NavigationBar";
 import Footer from "@/components/footer/Footer";
 import { usePathname} from "next/navigation";
+import Contact from "@/components/contact-us/contact-us";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
           <NavigationBar />
           {children}
           <Footer />
+          <Contact />
         </main>
       </body>
     </html>
