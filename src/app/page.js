f133ad204b2356acfa381ab648ec3108fd25eaa9
Rahul Rasve <rahulrasve712@gmail.com>
2024-01-17 13:44:45 +0530

    hot-to-invest component in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HomeBanner from "@/components/homepage/HomeBanner";
 import Recommendations from "@/components/homepage/Recommendations";
+import HowToInvest from "@/components/homepage/How_to_invest";
 
 export default function Home() {
 	return (
 		<main className="w-full bg-white min-h-screen">
 			<HomeBanner />
 			<Recommendations />
+			<HowToInvest />
 		</main>
 	);
 }
