4b4a3ecca61a7b2b8c236dc226b132f6b004fdd2
sumith prabhu <sumithprabhu07@gmail.com>
2024-05-12 23:12:15 +0530

    closing soon logic implemented

--- src/components/homepage/ClosingSoon.jsx
+++ src/components/homepage/ClosingSoon.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 import StockWidget from "./StockWidget";
 
 const ClosingSoon = ({ title }) => {
-	const data = [
-		{
-			image: `/recomendation_images/c_one.webp`,
-			name: `WiGL`,
-			review: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
-		},
-		{
-			image: `/recomendation_images/c_two.webp`,
-			name: `NERD Focus`,
-			review: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
-		},
-		{
-			image: `/recomendation_images/c_three.webp`,
-			name: `ACME AtronOmatic`,
-			review: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
-		},
+	const [data, setData] = useState([]);
+	
 
-	];
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/startups`)
+            .then(response => response.json())
+            .then(data => {
+                const transformedData = data.data.map(item => ({
+					id: item.id,
+                    image: item.attributes.displayImg, // Static image, consider dynamic handling if needed
+                    name: item.attributes.startupName,
+                    subtitle: item.attributes.subtitle,
+                    raised: item.attributes.currentFunding, // Adjust formatting as needed
+                    investors: item.attributes.noInvestors,
+                    mininvestment: item.attributes.minInvestment,
+					endDate: item.attributes.endDate,
+					summary: item.attributes.summary
+                }))
+				.sort((a, b) => new Date(a.endDate) - new Date(b.endDate)) // Sort by endDate ascending
+                .slice(0, 3);
+				
+                setData(transformedData);
+				console.log(data);
+				setTotalEntries(data.meta.pagination.total);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, []); 
 	return (
 		<div className="w-full bg-gray-100 ">
 			<div className="w-4/5 m-auto py-14">
@@ -29,7 +38,7 @@ const ClosingSoon = ({ title }) => {
 				</h1>
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-7">
 					{data.map((e, index) => (
-						<StockWidget key={index} e={e} />
+						<StockWidget key={index} id={e.id} image={e.image} name={e.name} summary={e.summary} isClosingSoon={true} endDate={e.endDate}/>
 					))}
 				</div>
 			</div>
